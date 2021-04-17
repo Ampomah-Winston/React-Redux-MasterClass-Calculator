08f40a7c4109e7b843787d33c88fd86a3d0ade5b
proau <ampomah.winston@amalitech.org>
2021-04-17 06:53:49 -0300

    add

--- src/myComponent/Display/index.js
+++ src/myComponent/Display/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export const Display = ({text,clickHandler}) =>(
     <div className="display" onClick={clickHandler?clickHandler:null}>
         {text.toString().split(' ').reverse().join(' ')}
